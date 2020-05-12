3d5c9bb3e44a09ab28e538e8ed7935cebe1deb67
Josh Schuster <joshuaschusterjs@gmail.com>
2020-05-11 22:57:51 -0400

    Removed security risk

--- src/utils/api/newsApi.js
+++ src/utils/api/newsApi.js
@@ -2,9 +2,7 @@ const baseUrl = 'https://newsapi.org/v2/top-headlines';
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export const getArticles = async (countryCode) => {
-  const res = await fetch(
-    `${baseUrl}?country=${countryCode}&apiKey=580fb6680fa2448ca2c768c5c10215c7`
-  );
+  const res = await fetch(`${baseUrl}?country=${countryCode}&apiKey=${apiKey}`);
   const json = await res.json();
 
   return json.articles;
