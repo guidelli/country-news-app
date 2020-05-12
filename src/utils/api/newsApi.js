fe97bb7856b133bab8e6606e2a91d7c88084ab52
Josh Schuster <joshuaschusterjs@gmail.com>
2020-05-11 22:55:21 -0400

    Finished production build

--- src/utils/api/newsApi.js
+++ src/utils/api/newsApi.js
@@ -2,7 +2,9 @@ const baseUrl = 'https://newsapi.org/v2/top-headlines';
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export const getArticles = async (countryCode) => {
-  const res = await fetch(`${baseUrl}?country=${countryCode}&apiKey=${apiKey}`);
+  const res = await fetch(
+    `${baseUrl}?country=${countryCode}&apiKey=580fb6680fa2448ca2c768c5c10215c7`
+  );
   const json = await res.json();
 
   return json.articles;
